937e6f5a8bcc7189e35ffc7615afbc597fe70fe4
Ratheesh <rpadinjareambattu@opentrends.net>
2019-01-07 13:26:45 +0530

    VueX

--- VueJS/routing/src/components/Header.vue
+++ VueJS/routing/src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
     <ul class="nav nav-pills">
         <router-link tag="li" to="/" active-class="active" exact>
-        <a>Home</a>
+            <a>Home</a>
         </router-link>
-        <router-link tag="li" to="/user" active-class="active"><a>User</a></router-link>
+        <router-link tag="li" to="/user" active-class="active">
+            <a>User</a>
+        </router-link>
     </ul>
 </template>
