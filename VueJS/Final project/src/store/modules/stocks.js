1fd2ff67dd1304850bc397f69add74d052d0f8a8
Ratheesh <rpadinjareambattu@opentrends.net>
2019-01-08 17:25:13 +0530

    added random price for stock and global filter for currency

--- VueJS/Final project/src/store/modules/stocks.js
+++ VueJS/Final project/src/store/modules/stocks.js
@@ -8,8 +8,10 @@ const mutations = {
     'SET_STOCKS' (stete, stocks) {
         state.stocks = stocks;
     },
-    'RND_STOCKS' (stete) {
-
+    'RND_STOCKS' (state) {
+        state.stocks.forEach(stock => {
+            stock.price = Math.round(stock.price * (1 + Math.random() - 0.5));
+        })
     }
 }
 
