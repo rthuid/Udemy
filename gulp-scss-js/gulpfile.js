ae4586f764197b310e8e42777b9907427b575d2f
Ratheesh <rpadinjareambattu@opentrends.net>
2019-01-18 15:46:57 +0530

    modified dist & src paths

--- gulp-scss-js/gulpfile.js
+++ gulp-scss-js/gulpfile.js
@@ -15,25 +15,27 @@ var imagemin = require('gulp-imagemin');
 var imageminPngQuant = require('imagemin-pngquant');
 var imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
-// common dest path
-var DIST_PATH = 'public/dist';
-var DIST_PATH_CSS = 'public/css';
-var DIST_PATH_SCRIPTS = 'public/js';
-var DIST_PATH_IMAGES = 'public/images';
-// file path
-var SCRIPTS_PATH = [
-    'mockups/scripts/vendor/jquery-3.3.1.min.js',
-    'mockups/scripts/**/*.js'
-];
-var IMAGES_PATH = 'mockups/images/**/*.{png,jpeg,jpg,svg,gif}';
-var SCSS_PATH = 'mockups/scss/**/*.scss';
-var HTML_PATH = 'public/**/*.html';
+var SRC_PATH = './src/';
+var DIST_PATH = './public/';
+var PATHS = {
+    DIST_SCSS: DIST_PATH + 'css',
+    DIST_SCRIPTS: DIST_PATH + 'js',
+    DIST_IMAGES: DIST_PATH + 'images',
+    DIST_HTML: DIST_PATH,
+
+    SRC_SCRIPTS: [
+        SRC_PATH + 'scripts/vendor/jquery-3.3.1.min.js',
+        SRC_PATH + 'scripts/**/*.js'
+    ],
+    SRC_IMAGES: SRC_PATH + 'images/**/*.{png,jpeg,jpg,svg,gif}',
+    SRC_SCSS: SRC_PATH + 'scss/**/*.scss',
+    SRC_HTML: SRC_PATH + '**/*.html',
+}
 
 // styles for scss
 gulp.task('styles', () => {
     console.log('starting style task');
-
-    return gulp.src('mockups/scss/styles.scss')
+    return gulp.src(SRC_PATH + 'scss/styles.scss')
     .pipe(plumber((err) => {
         console.log('Style Error');
         console.log(err);
@@ -45,14 +47,13 @@ gulp.task('styles', () => {
         outputStyle: 'compressed'
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(DIST_PATH_CSS))
+    .pipe(gulp.dest(PATHS.DIST_SCSS))
     .pipe(livereload())
 });
 
-
 // scripts
 gulp.task('scripts', () => {
-    return gulp.src(SCRIPTS_PATH)
+    return gulp.src(PATHS.SRC_SCRIPTS)
     .pipe(plumber((err) => {
         console.log('Script Error');
         console.log(err);
@@ -65,20 +66,20 @@ gulp.task('scripts', () => {
     .pipe(uglify())
     .pipe(concat('scripts.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(DIST_PATH_SCRIPTS))
+    .pipe(gulp.dest(PATHS.DIST_SCRIPTS))
     .pipe(livereload())
 });
 
 // html - (only live reload for html files)
 gulp.task('html', () => {
-    return gulp.src(HTML_PATH)
-    .pipe(gulp.dest(''))
+    return gulp.src(PATHS.SRC_HTML)
+    .pipe(gulp.dest(PATHS.DIST_HTML))
     .pipe(livereload())
 })
 
 // images
 gulp.task('images', () => {
-    return gulp.src(IMAGES_PATH)
+    return gulp.src(PATHS.SRC_IMAGES)
     .pipe(imagemin(
         [
             imagemin.gifsicle(),
@@ -89,18 +90,18 @@ gulp.task('images', () => {
             imageminJpegRecompress()
         ]
     ))
-    .pipe(gulp.dest(DIST_PATH_IMAGES));
+    .pipe(gulp.dest(PATHS.DIST_IMAGES));
 });
 
 // clean|delete
 gulp.task('clean', () => {
     return del.sync([
-        DIST_PATH, DIST_PATH_SCRIPTS, DIST_PATH_CSS, DIST_PATH_IMAGES
+        PATHS.DIST_HTML, PATHS.DIST_IMAGES, PATHS.DIST_SCRIPTS, PATHS.DIST_SCSS
     ])
 })
 
 // default
-gulp.task('default', ['clean', 'images', 'styles', 'scripts'], () => {
+gulp.task('default', ['html','clean', 'images', 'styles', 'scripts'], () => {
     console.log('starting default task');
 });
 
@@ -109,9 +110,9 @@ gulp.task('watch',['default'], ()=> {
     console.log('Starting watch task');
     require('./server.js');
     livereload.listen();
-    gulp.watch(SCRIPTS_PATH, ['scripts']);
-    gulp.watch(SCSS_PATH, ['styles']);
-    gulp.watch('./public/**/*.html', ['html']);
+    gulp.watch(PATHS.SRC_SCRIPTS, ['scripts']);
+    gulp.watch(PATHS.SRC_SCSS, ['styles']);
+    gulp.watch(PATHS.SRC_HTML, ['html']);
 });
 
 
